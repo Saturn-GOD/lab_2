35b684eeb6ac5270a98c3bc547c8991c2c7aca3e
Alrighter <59889888+Alrighter@users.noreply.github.com>
2021-05-30 18:59:07 +0300

    m

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<string, int> Ages = new MyDict<string, int>();
+            Ages.Add("Roman", 19);
+            Console.WriteLine(Ages["Roman"]);
+        }
+    }
+}
